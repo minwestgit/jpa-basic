2147df4e5aba9dea6a7ed7b4ab5f658e984540aa
minwestgit <minwest61@gmail.com>
2021-12-26 21:42:27 +0900

    First Commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 하나만 생성해 App 전체가 공유
+        EntityManager em = emf.createEntityManager(); // 쓰레드간 공유 절대 금지!
+
+        // 트랜잭션 처리 (JPA의 모든 데이터 변경은 트랜잭션 안에서 진행)
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            /*
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+             */
+            Member fineMember = em.find(Member.class, 1L);
+            System.out.println("Id" + fineMember.getId());
+            System.out.println("Name" + fineMember.getName());
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
